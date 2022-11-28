d014424989c8e5bac8c47d7e32b9240d07d53020
Abhinav <abhinav@AVs-Mac.local>
2022-11-28 23:39:11 +0530

    [28 Nov 2022] Class based components, lifecycle of components, event handlers of class based components.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { FirstClassComp } from './components/FirstClassComp';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React App</h1>
+        <FirstClassComp/>
       </header>
     </div>
   );
