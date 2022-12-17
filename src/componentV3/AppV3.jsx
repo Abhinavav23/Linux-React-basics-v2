5ae2b9cb7662ce80bf4f091ebe5b23fee1f9cc5b
Abhinav <abhinav@AVs-Mac.local>
2022-12-17 17:18:04 +0530

    [16 dec 2022] custom hooks

---
+++ src/componentV3/AppV3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+import { FirstCompWithCounter } from './FirstCompWithCounter'
+import { SecondCompWithCounter } from './SecondCompWithCounter'
+export const AppV3 = () => {
+  return (
+    <>
+        <h2>AppV3</h2>
+        <FirstCompWithCounter/>
+        <SecondCompWithCounter/>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppV2 } from './AppV2';
+import { AppV3 } from './componentV3/AppV3';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AppV2/>
+    <AppV3/>
 );
